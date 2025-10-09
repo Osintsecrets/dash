1cf52002e9ec063b2dbe6346c8e85a6c7f55ddf2
Osintsecrets <osintsecrets@proton.me>
2025-10-09 12:35:10 +0300

    Add animated global background component

---
+++ components/ui/AnimatedBackground.tsx
@@ -0,0 +1,6 @@
+'use client';
+import styles from './AnimatedBackground.module.css';
+
+export default function AnimatedBackground() {
+  return <div className={styles.container} aria-hidden="true" />;
+}
